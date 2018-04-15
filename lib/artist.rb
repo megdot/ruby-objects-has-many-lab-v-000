1948dcfd4fc02288f71b76e9bb8ba80d3119b989
Megan Dotto <megan.dotto@gmail.com>
2018-04-15 04:57:06 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  attr_accessor :name, :songs
+  @@song_count = 0
+
+  def initialize(name)
+    @name = name
+    @song = []
+  end
+
+  def add_song(song)
+    @songs << song
+    @@song_count += 1
+    song.artist = self
+  end
+
+
+end

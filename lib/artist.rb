64fa5d850b7695ba3e8247e39914b2e90de2d406
Megan Dotto <megan.dotto@gmail.com>
2018-04-15 05:07:51 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,6 +10,18 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name
+  end
+
+  def songs
+    @songs
+  end
+
+  def self.song_count
+    @@song_count
   end
 
 
